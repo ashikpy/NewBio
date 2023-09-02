7972356cbf19dce9dbe0afd44e28cab77638e8e1
Ash D <ashikofficial455@gmail.com>
2023-09-02 11:45:38 +0530

    Major Update

---
+++ main.js
@@ -0,0 +1,6 @@
+//print hello using js in html
+const h = require('h');
+console.log('Hello from JavaScript!');
+module.exports = () => h('p', {}, 'Hello from JavaScript!');
+
+//does this print hello in html
